981e5ec328c7d1b1416c89bdff7c633c5a577728
tinhpx <phamtinh142@gmail.com>
2024-06-24 01:29:27 +0700

    remove package

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,8 +5,7 @@ import { SocketModule } from '@providers/socket/socket.module';
 import { DatabaseModule } from '@providers/database/database.module';
 import { AppConfigModule } from '@configs/app/app.module';
 import { RedisConfigModule } from '@configs/redis/redis.module';
-import * as csurf from 'csurf';
-import { ControllerModule } from './controllers/index.module';
+import { ControllerModule } from '@controllers/index.module';
 
 @Global()
 @Module({
@@ -14,13 +13,5 @@ import { ControllerModule } from './controllers/index.module';
   providers: [AppService],
   exports: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(csurf({ cookie: true }))
-      .forRoutes(
-        { path: 'auth/signup', method: RequestMethod.POST },
-        { path: 'auth/login', method: RequestMethod.POST },
-      );
-  }
+export class AppModule {
 }
